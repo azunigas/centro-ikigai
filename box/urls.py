4bb572ab83f78b1bb1a8f03412e273965f8abfd5
Alvaro <alvarohzuniga@gmail.com>
2019-09-03 18:02:54 -0400

    reservas

--- box/urls.py
+++ box/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'box'
 
 urlpatterns = [
-    path('', views.BoxView.as_view(), name='boxview'),
+    path('', views.BoxView.as_view(), name='index'),
     path('mensaje/', views.consultaView, name='consulta')
 ]
